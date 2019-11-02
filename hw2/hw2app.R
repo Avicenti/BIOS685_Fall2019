02daa3ee65f82e07501642d0bcce6419a36fbba5
Avicenti <avicenti@email.arizona.edu>
2019-11-01 23:43:45 -0700

    Update hw2app.R

--- hw2/hw2app.R
+++ hw2/hw2app.R
@@ -9,7 +9,7 @@
 
 library(shiny)
 
-DF <- read_rds("Indeed.rds")
+DF <- readRDS("Indeed.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -19,8 +19,8 @@ ui <- fluidPage(
 
     selectInput("city", 
                 label = "Choose a city",
-                choices = c("New York", "Los Angeles",
-                            "Chicago", "Houston","Phoenix"),
+                choices = c("New York, NY", "Los Angeles, CA",
+                            "Chicago, IL", "Houston, TX","Phoenix, AZ"),
                 selected = "New York"),
     
     selectInput("var", 
@@ -41,24 +41,34 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$distPlot1 <- renderPlot({
-        if(input$var== "Number of Jobs"){
+        if(input$var== "Job Ratings"){
             # generate bins based on input$bins from ui.R
-            x    <- DF$company_review
-            bins <- seq(min(x), max(x), length.out = 20)
-            
+            x <- subset(DF, job_location==input$city) 
+            bins <- seq(0,5,.5)
             # draw the histogram with the specified number of bins
-            hist(x, breaks = bins, col = 'darkgray', border = 'white')
+            hist(x$company_review, col = 'darkgray', border = 'white', breaks = bins,main=input$city, xlab="Company Ratings" )
+        }
+        
+        if(input$var== "Number of Jobs"){
+            ggplot(data = DF) + 
+                geom_bar(mapping = aes(x = DF$job_location))
         }
 
     })
     
     output$distPlot2 <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- rexp(100)
-        bins <- seq(min(x), max(x), length.out = 20)
         
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        if(input$var== "Job Ratings"){
+            
+            City<-c("New York, NY", "Los Angeles, CA","Chicago, IL", "Houston, TX","Phoenix, AZ")
+            # generate bins based on input$bins from ui.R
+            Average_Rating <- c(mean(subset(DF, job_location=="New York, NY")$company_review,na.rm=TRUE),mean(subset(DF, job_location=="Los Angeles, CA")$company_review,na.rm=TRUE),
+                   mean(subset(DF, job_location=="Chicago, IL")$company_review,na.rm=TRUE),mean(subset(DF, job_location=="Houston, TX")$company_review,na.rm=TRUE),
+                   mean(subset(DF, job_location=="Phoenix, AZ")$company_review,na.rm=TRUE))
+            df2<-data.frame(City,Average_Rating)
+            # draw the histogram with the specified number of bins
+            ggplot(df2,aes(City,Average_Rating)) +geom_col()+labs(title = "Average Employer Rating By City")
+        }
     })
 }
 
